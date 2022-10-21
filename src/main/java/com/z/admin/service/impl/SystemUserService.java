e53ccf893b957cc2e20f29ca22c8135ab1d5ab2e
zhy <1246230855@qq.com>
2022-10-21 17:13:22 +0800

    test

--- src/main/java/com/z/admin/service/impl/SystemUserService.java
+++ src/main/java/com/z/admin/service/impl/SystemUserService.java
@@ -1,9 +1,7 @@
 package com.z.admin.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.z.admin.dao.SystemUserMapper;
 import com.z.admin.entity.param.system.UserQueryParam;
