59e6ded8d3fb6addb4245af34e385c945d05dcdd
zhy <1246230855@qq.com>
2023-12-07 11:26:57 +0800

    代码优化

---
+++ src/main/java/com/z/admin/config/SecurityConfigBK.java
@@ -0,0 +1,90 @@
+//package com.z.admin.config;
+//
+//import com.z.admin.filter.LoginFilter;
+//import com.z.admin.security.MyEntryPoint;
+//import com.z.admin.service.impl.SystemUserService;
+//import jakarta.annotation.Resource;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+///**
+// * @author zhy
+// * @description
+// * @date 2022/12/21
+// */
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfigBK {
+//
+//    @Resource
+//    SystemUserService userDetailsService;
+//    @Resource
+//    LoginFilter loginFilter;
+//    @Resource
+//    MyEntryPoint myEntryPoint;
+//
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.csrf(AbstractHttpConfigurer::disable)
+//                .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+//                // 开启跨域以便前端调用接口
+//                .cors(Customizer.withDefaults())
+//                //关闭session
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                // 将我们自定义的认证过滤器插入到默认的认证过滤器之前
+//                .addFilterBefore(loginFilter, UsernamePasswordAuthenticationFilter.class)
+//                .exceptionHandling(e -> e.authenticationEntryPoint(myEntryPoint).configure(http))
+//
+//        ;
+//
+//        http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+//                        // 允许所有OPTIONS请求
+//                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+//                        // 允许直接访问授权登录接口
+//                        .requestMatchers(HttpMethod.POST, "/system/user/login", "/test/register").permitAll()
+//                        // 这里意思是其它所有接口需要认证才能访问
+//                        .anyRequest().authenticated())
+//                .authenticationProvider(authenticationProvider())
+//                .exceptionHandling(exceptionHandlingConfigurer -> exceptionHandlingConfigurer.authenticationEntryPoint(myEntryPoint));
+//
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+//        return config.getAuthenticationManager();
+//    }
+//
+//    @Bean
+//    public AuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(userDetailsService);
+//        // 设置密码编辑器
+//        authProvider.setPasswordEncoder(passwordEncoder());
+//        return authProvider;
+//
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//}
