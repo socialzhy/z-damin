1ccdfd0521fd798e652092f227ee8fd1887d35f4
zhy <1246230855@qq.com>
2025-03-07 14:25:45 +0800

    xx

--- z_admin.sql
+++ z_admin.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80028 (8.0.28)
  File Encoding         : 65001
 
- Date: 26/12/2024 14:33:02
+ Date: 07/03/2025 14:25:18
 */
 
 SET NAMES utf8mb4;
@@ -23,13 +23,13 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `system_permission`;
 CREATE TABLE `system_permission` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-  `method` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT 'POST' COMMENT '请求方法',
-  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '操作路由',
+  `method` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'POST' COMMENT '请求方法',
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '操作路由',
   `type` int NOT NULL DEFAULT '1' COMMENT '0超管权限 1页面权限 2操作权限',
   `parent_id` bigint NOT NULL DEFAULT '0' COMMENT '权限父节点id',
   `access_level` int NOT NULL DEFAULT '0' COMMENT '权限等级 1允许匿名访问  2登录即可访问  3须有此权限可访问',
-  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '权限名称',
-  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '描述',
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '权限名称',
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '描述',
   `create_by` bigint NOT NULL DEFAULT '0' COMMENT '创建人',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_by` bigint NOT NULL DEFAULT '0' COMMENT '修改人',
@@ -37,7 +37,7 @@ CREATE TABLE `system_permission` (
   `deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否删除',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `uni_method_path` (`method`,`path`)
-) ENGINE=InnoDB AUTO_INCREMENT=100004 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='权限';
+) ENGINE=InnoDB AUTO_INCREMENT=100004 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='权限';
 
 -- ----------------------------
 -- Records of system_permission
@@ -49,7 +49,7 @@ INSERT INTO `system_permission` (`id`, `method`, `path`, `type`, `parent_id`, `a
 INSERT INTO `system_permission` (`id`, `method`, `path`, `type`, `parent_id`, `access_level`, `name`, `description`, `create_by`, `create_time`, `update_by`, `update_time`, `deleted`) VALUES (1001, 'POST', '/system/user/add', 2, 11, 3, '新增用户', '新增用户', 0, '2023-01-06 17:19:30', 0, '2023-01-06 17:19:30', 0);
 INSERT INTO `system_permission` (`id`, `method`, `path`, `type`, `parent_id`, `access_level`, `name`, `description`, `create_by`, `create_time`, `update_by`, `update_time`, `deleted`) VALUES (1002, 'GET', '/system/user/{id}', 2, 11, 3, '用户详情', '用户详情', 0, '2023-01-06 17:19:30', 0, '2023-01-06 17:19:30', 0);
 INSERT INTO `system_permission` (`id`, `method`, `path`, `type`, `parent_id`, `access_level`, `name`, `description`, `create_by`, `create_time`, `update_by`, `update_time`, `deleted`) VALUES (9999, 'GET', '/test/test', 2, 11, 1, '接口测试', '接口测试', 0, '2023-01-06 17:19:30', 0, '2023-01-06 17:19:30', 0);
-INSERT INTO `system_permission` (`id`, `method`, `path`, `type`, `parent_id`, `access_level`, `name`, `description`, `create_by`, `create_time`, `update_by`, `update_time`, `deleted`) VALUES (10001, 'POST', '/system/user/login', 2, 11, 1, '登录', '登录', 0, '2023-01-06 17:19:30', 0, '2023-01-06 17:19:30', 0);
+INSERT INTO `system_permission` (`id`, `method`, `path`, `type`, `parent_id`, `access_level`, `name`, `description`, `create_by`, `create_time`, `update_by`, `update_time`, `deleted`) VALUES (10001, 'POST', '/login', 2, 11, 1, '登录', '登录', 0, '2023-01-06 17:19:30', 0, '2023-01-06 17:19:30', 0);
 COMMIT;
 
 -- ----------------------------
@@ -66,7 +66,7 @@ CREATE TABLE `system_role` (
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
   `deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否删除',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='角色';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='角色';
 
 -- ----------------------------
 -- Records of system_role
@@ -90,7 +90,7 @@ CREATE TABLE `system_role_permission` (
   `deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否删除',
   PRIMARY KEY (`id`) USING BTREE,
   KEY `idx_roleId` (`role_id`) USING BTREE COMMENT '角色id'
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='角色权限关联';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='角色权限关联';
 
 -- ----------------------------
 -- Records of system_role_permission
@@ -105,11 +105,11 @@ COMMIT;
 DROP TABLE IF EXISTS `system_user`;
 CREATE TABLE `system_user` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-  `mobile` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '手机号',
-  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '邮箱',
-  `username` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '用户名',
-  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '密码',
-  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '姓名',
+  `mobile` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '手机号',
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '邮箱',
+  `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '用户名',
+  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '密码',
+  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '姓名',
   `create_by` bigint NOT NULL DEFAULT '0' COMMENT '创建人',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_by` bigint NOT NULL DEFAULT '0' COMMENT '修改人',
@@ -143,7 +143,7 @@ CREATE TABLE `system_user_permission` (
   `deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否删除',
   PRIMARY KEY (`id`),
   KEY `idx_uerId` (`user_id`) USING BTREE COMMENT '用户id'
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='用户权限关联';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户权限关联';
 
 -- ----------------------------
 -- Records of system_user_permission
@@ -168,7 +168,7 @@ CREATE TABLE `system_user_role` (
   `deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否删除',
   PRIMARY KEY (`id`),
   KEY `idx_userId` (`user_id`) USING BTREE COMMENT '用户id'
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='用户角色关联';
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户角色关联';
 
 -- ----------------------------
 -- Records of system_user_role
